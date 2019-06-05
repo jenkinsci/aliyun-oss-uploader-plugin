08f4e3fb16f279860834ab184474417faf0aa54e
raylax <raylax@inurl.org>
2019-06-05 22:16:39 +0800

    update

--- src/main/java/org/inurl/jenkins/plugin/OSSPublisher.java
+++ src/main/java/org/inurl/jenkins/plugin/OSSPublisher.java
@@ -1,24 +1,26 @@
 package org.inurl.jenkins.plugin;
 
+import com.aliyun.oss.OSSClient;
+import hudson.Extension;
 import hudson.FilePath;
 import hudson.Launcher;
+import hudson.model.AbstractProject;
 import hudson.model.Run;
 import hudson.model.TaskListener;
-import hudson.util.FormValidation;
-import jenkins.tasks.SimpleBuildStep;
-import org.apache.commons.lang.StringUtils;
-import org.inurl.jenkins.plugin.Messages;
-
-import hudson.Extension;
-import hudson.model.AbstractProject;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.BuildStepMonitor;
 import hudson.tasks.Publisher;
+import hudson.util.FormValidation;
+import jenkins.tasks.SimpleBuildStep;
+import org.apache.commons.lang.StringUtils;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.QueryParameter;
 
 import javax.annotation.Nonnull;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 
 public class OSSPublisher extends Publisher implements SimpleBuildStep {
 
@@ -76,39 +78,68 @@ public class OSSPublisher extends Publisher implements SimpleBuildStep {
 
     @Override
     public void perform(@Nonnull Run<?, ?> run, @Nonnull FilePath workspace, @Nonnull Launcher launcher, @Nonnull TaskListener listener) throws InterruptedException, IOException {
-        listener.getLogger().println("endpoint          => " + endpoint + "!");
-        listener.getLogger().println("accessKeyId       => " + accessKeyId + "!");
-        listener.getLogger().println("accessKeySecret   => " + accessKeySecret + "!");
-        listener.getLogger().println("bucketName        => " + bucketName + "!");
-        listener.getLogger().println("localPath         => " + localPath + "!");
-        listener.getLogger().println("remotePath        => " + remotePath + "!");
+        PrintStream logger = listener.getLogger();
+        OSSClient client = new OSSClient(endpoint, accessKeyId, accessKeySecret);
+        String local = localPath.substring(1);
+        String remote = remotePath.substring(1);
+        logger.println("workspace => " + workspace);
+        FilePath p = new FilePath(workspace, local);
+        if (p.isDirectory()) {
+            logger.println("upload dir => " + p);
+            upload(client, logger, remote, p, true);
+        } else {
+            logger.println("upload file => " + p);
+            uploadFile(client, logger, remote, p);
+        }
+    }
+
+    private void upload(OSSClient client, PrintStream logger, String base, FilePath path, boolean root) throws InterruptedException, IOException {
+        if (path.isDirectory()) {
+            for (FilePath f : path.list()) {
+                upload(client, logger, base + (root ? "" : ("/" +  path.getName())), f, false);
+            }
+            return;
+        }
+        uploadFile(client, logger, base + "/" + path.getName(), path);
+    }
+
+    private void uploadFile(OSSClient client, PrintStream logger, String key, FilePath path) throws InterruptedException, IOException {
+        String realKey = key;
+        if (realKey.startsWith("/")) {
+            realKey = realKey.substring(1);
+        }
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        path.copyTo(outputStream);
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+        logger.println("uploading [" + path.getRemote() + "] to [" + realKey + "]");
+        client.putObject(bucketName, realKey, inputStream);
     }
 
     @Extension
     public static final class DescriptorImpl extends BuildStepDescriptor<Publisher> {
 
-        public FormValidation doCheckEndpoint(@QueryParameter String value) {
+        public FormValidation doCheckEndpoint(@QueryParameter(required = true) String value) {
             return checkValue(value, Messages.OSSPublish_MissingEndpoint());
         }
 
-        public FormValidation doCheckAccessKeyId(@QueryParameter String value) {
+        public FormValidation doCheckAccessKeyId(@QueryParameter(required = true) String value) {
             return checkValue(value, Messages.OSSPublish_MissingAccessKeyId());
         }
 
-        public FormValidation doCheckAccessKeySecret(@QueryParameter String value) {
+        public FormValidation doCheckAccessKeySecret(@QueryParameter(required = true) String value) {
             return checkValue(value, Messages.OSSPublish_MissingAccessKeySecret());
         }
 
 
-        public FormValidation doCheckBucketName(@QueryParameter String value) {
+        public FormValidation doCheckBucketName(@QueryParameter(required = true) String value) {
             return checkValue(value, Messages.OSSPublish_MissingBucketName());
         }
 
-        public FormValidation doCheckLocalPath(@QueryParameter String value) {
+        public FormValidation doCheckLocalPath(@QueryParameter(required = true) String value) {
             return checkValue(value, Messages.OSSPublish_MissingLocalPath());
         }
 
-        public FormValidation doCheckRemotePath(@QueryParameter String value) {
+        public FormValidation doCheckRemotePath(@QueryParameter(required = true) String value) {
             return checkValue(value, Messages.OSSPublish_MissingRemotePath());
         }
 
